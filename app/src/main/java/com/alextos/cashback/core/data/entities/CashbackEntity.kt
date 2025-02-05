520bfc56646e8eb2b25c9e0f99bd25f2fcbddc27
aleks100s <aleksandr100s@yandex.ru>
2025-02-05 11:02:23 +0700

    Setup foreign keys

--- app/src/main/java/com/alextos/cashback/core/data/entities/CashbackEntity.kt
+++ app/src/main/java/com/alextos/cashback/core/data/entities/CashbackEntity.kt
@@ -1,9 +1,28 @@
 package com.alextos.cashback.core.data.entities
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "cashback")
+@Entity(
+    tableName = "cashback",
+    foreignKeys = [
+        ForeignKey(
+            entity = CardEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["cardId"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = CategoryEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["categoryId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index("cardId"), Index("categoryId")]
+)
 data class CashbackEntity(
     @PrimaryKey(autoGenerate = false)
     val id: String,
--- app/src/main/java/com/alextos/cashback/core/data/entities/PlaceEntity.kt
+++ app/src/main/java/com/alextos/cashback/core/data/entities/PlaceEntity.kt
@@ -1,9 +1,23 @@
 package com.alextos.cashback.core.data.entities
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
+import com.alextos.cashback.core.domain.Category
 
-@Entity(tableName = "places")
+@Entity(
+    tableName = "places",
+    foreignKeys = [
+        ForeignKey(
+            entity = CategoryEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["categoryId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index("categoryId")]
+)
 data class PlaceEntity(
     @PrimaryKey(autoGenerate = false)
     val id: String,
