8a3d6987d86fe9f1e6204d4f2f260da3a1cba518
aleks100s <aleksandr100s@yandex.ru>
2025-03-09 12:46:51 +0700

    Increase app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.alextos.cashback"
         minSdk = 28
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.6"
+        versionCode = 12
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
