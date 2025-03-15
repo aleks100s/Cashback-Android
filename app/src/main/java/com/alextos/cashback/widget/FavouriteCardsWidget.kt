4368b22dc040cef420fbccbf01e784561d76fcc5
aleks100s <aleksandr100s@yandex.ru>
2025-03-15 13:45:11 +0700

    Widget layout

--- app/src/main/java/com/alextos/cashback/widget/FavouriteCardsWidget.kt
+++ app/src/main/java/com/alextos/cashback/widget/FavouriteCardsWidget.kt
@@ -1,6 +1,8 @@
 package com.alextos.cashback.widget
 
 import android.content.Context
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
@@ -12,13 +14,18 @@ import androidx.glance.appwidget.lazy.items
 import androidx.glance.appwidget.provideContent
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
+import androidx.glance.layout.Column
+import androidx.glance.layout.Spacer
 import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.padding
+import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import com.alextos.cashback.R
 import com.alextos.cashback.core.domain.models.Card
 import com.alextos.cashback.core.domain.repository.CardRepository
+import com.alextos.cashback.widget.views.CardItemView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.koin.java.KoinJavaComponent.inject
@@ -42,12 +49,31 @@ class FavouriteCardsWidget : GlanceAppWidget() {
                             )
                         }
                     } else {
-                        LazyColumn(modifier = GlanceModifier.padding(16.dp)) {
-                            items(cards) { card ->
-                                Text(
-                                    text = card.name,
-                                    style = TextStyle(color = GlanceTheme.colors.primary),
+                        Column(
+                            modifier = GlanceModifier
+                                .padding(top = 8.dp)
+                                .fillMaxWidth()
+                        ) {
+                            Text(
+                                modifier = GlanceModifier.padding(8.dp),
+                                text = context.getString(R.string.widget_favourite_cards),
+                                style = TextStyle(
+                                    color = GlanceTheme.colors.primary,
+                                    fontSize = TextUnit(20f, TextUnitType.Sp),
+                                    fontWeight = FontWeight.Bold
                                 )
+                            )
+
+                            LazyColumn {
+                                items(cards) { card ->
+                                    Column(modifier = GlanceModifier.fillMaxWidth()) {
+                                        CardItemView(card = card)
+
+                                        if (card != cards.lastOrNull()) {
+                                            Spacer(modifier = GlanceModifier.padding(4.dp))
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
---
+++ app/src/main/java/com/alextos/cashback/widget/views/CardItemView.kt
@@ -0,0 +1,52 @@
+package com.alextos.cashback.widget.views
+
+import androidx.glance.layout.Box
+import androidx.glance.layout.Column
+import androidx.glance.text.Text
+import androidx.compose.runtime.Composable
+import androidx.glance.layout.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
+import androidx.glance.appwidget.cornerRadius
+import androidx.glance.background
+import androidx.glance.layout.fillMaxWidth
+import androidx.glance.layout.padding
+import androidx.glance.text.FontWeight
+import androidx.glance.text.TextStyle
+import com.alextos.cashback.common.makeColor
+import com.alextos.cashback.core.domain.models.Card
+
+@Composable
+fun CardItemView(
+    modifier: GlanceModifier = GlanceModifier,
+    card: Card
+) {
+    Box(
+        modifier = modifier
+            .cornerRadius(16.dp)
+            .background(color = makeColor(card.color).copy(0.4f))
+            .fillMaxWidth()
+    ) {
+        Column(
+            modifier = GlanceModifier
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.Start,
+        ) {
+            Text(
+                modifier = GlanceModifier.padding(bottom = 8.dp),
+                text = card.name,
+                style = TextStyle(
+                    color = GlanceTheme.colors.onSurface,
+                    fontWeight = FontWeight.Bold
+                ),
+            )
+
+            Text(
+                text = card.toString(),
+                style = TextStyle(color = GlanceTheme.colors.onSurface)
+            )
+        }
+    }
+}
